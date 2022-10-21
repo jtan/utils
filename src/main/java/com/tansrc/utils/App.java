a0b7ff9902f736eb1ea4ee66593f4a95d0b598e6
John Tan <john@tansrc.com>
2022-10-21 18:10:24 -0400

    Updating demo to show merge sort

--- src/main/java/com/tansrc/utils/App.java
+++ src/main/java/com/tansrc/utils/App.java
@@ -1,13 +1,24 @@
 package com.tansrc.utils;
 
+import com.tansrc.utils.MergeSort;
+import java.util.Arrays;
+
 /**
- * Hello world!
+ * Demo App
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
+		
+		System.out.println("Demo: Use MergeSort on array");
+		int[] array = {2, 4, 6, 1, 5, 16, 4, 8};
+		
+		System.out.println("Test array: " + Arrays.toString(array));
+		MergeSort.process(array, 0, array.length - 1);
+		
+		System.out.println("MergeSort result: " + Arrays.toString(array)); 
     }
 }
